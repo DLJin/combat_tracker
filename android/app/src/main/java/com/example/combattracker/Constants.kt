a8ed6c5702f87391db034e9394c6b9cf7d4fa5b3
Nevin Mital <mital.nevin@gmail.com>
2021-08-21 19:43:13 -0700

    Initial commit for Firebase-based implementation
    Add initial files for Android app, webpage, and Firebase server.

---
+++ android/app/src/main/java/com/example/combattracker/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.combattracker
+
+object Constants {
+    private const val LOG_TAG = "CombatTracker"
+}
---
+++ android/app/src/main/java/com/example/combattracker/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.combattracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<LoginFragment>(R.id.fragment_container)
+        }
+    }
+}
