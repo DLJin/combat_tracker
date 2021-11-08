a0333ca2ba84c1ad49e94f0a32be27fc1235b769
Nevin Mital <mital.nevin@gmail.com>
2021-11-07 18:22:20 -0800

    [Compose] Add dropdown menu

--- android/app/src/main/java/com/example/combattracker/PlayerView.kt
+++ android/app/src/main/java/com/example/combattracker/PlayerView.kt
@@ -4,34 +4,105 @@ import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.combattracker.model.Entity
 import com.example.combattracker.ui.theme.CombatTrackerTheme
 
 class PlayerView : ComponentActivity() {
 
     val ixar = Entity(
+        name = "Ixar",
         attributes = Entity.Attributes(13, 12, 8, 10, 15, 14),
         movement = 30,
         healthFactor = 12.0,
         focusFactor = 6.5
     )
 
+    val seren = Entity(
+        name = "Seren",
+        attributes = Entity.Attributes(8, 12, 15, 14, 13, 12),
+        movement = 30,
+        healthFactor = 11.0,
+        focusFactor = 7.0
+    )
+
+    val elmo = Entity(
+        name = "Elmo Elless",
+        attributes = Entity.Attributes(13, 10, 14, 15, 12, 8),
+        movement = 45,
+        healthFactor = 13.0,
+        focusFactor = 6.0
+    )
+
+    val players = mutableListOf(ixar, seren, elmo)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
+            var expanded by remember { mutableStateOf(false) }
+            var selectedPlayer by remember { mutableStateOf(ixar) }
+            val icon = if (expanded) {
+                Icons.Filled.ArrowDropUp
+            } else {
+                Icons.Filled.ArrowDropDown
+            }
             CombatTrackerTheme {
-                DisplayPlayer(player = ixar)
+                Column {
+                    Button(
+                        onClick = {finish()},
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        Text("Back")
+                    }
+                    Box(
+                        modifier = Modifier.padding(8.dp)
+                    ) {
+                        OutlinedTextField(
+                            value = selectedPlayer.name,
+                            onValueChange = { },
+                            label = { Text("Player") },
+                            trailingIcon = {
+                                Icon(
+                                    icon,
+                                    "contentDescription",
+                                    Modifier.clickable { expanded = !expanded })
+                            },
+                            singleLine = true,
+                            readOnly = true
+                        )
+                        DropdownMenu(
+                            expanded = expanded,
+                            onDismissRequest = { expanded = false }
+                        ) {
+                            players.forEach { player ->
+                                DropdownMenuItem(onClick = {
+                                    selectedPlayer = player
+                                    expanded = false
+                                }) {
+                                    Text(text = player.name)
+                                }
+                            }
+                        }
+                    }
+                    DisplayPlayer(player = selectedPlayer)
+                }
             }
         }
     }
@@ -39,12 +110,15 @@ class PlayerView : ComponentActivity() {
 
 @Composable
 fun DisplayPlayer(player: Entity) {
-    val activity = LocalContext.current as? Activity
-    Column {
-        Button(onClick = {activity?.finish()}) {
-            Text("Back")
-        }
-        Row {
+    Column(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(
+            text = player.name
+        )
+        Row(
+            modifier = Modifier.padding(8.dp)
+        ) {
             Column(Modifier.weight(1f), horizontalAlignment = Alignment.CenterHorizontally) {
                 Text("CON", style=MaterialTheme.typography.overline)
                 Text(player.attributes.constitution.toString())
@@ -70,8 +144,14 @@ fun DisplayPlayer(player: Entity) {
                 Text(player.attributes.charisma.toString())
             }
         }
-        Text("HP: ${player.healthCurrent}/${player.healthTotal}")
-        Text("FP: ${player.focusCurrent}/${player.focusTotal}")
-        Text("Movement: ${player.movement}")
+        Text(
+            text = "HP: ${player.healthCurrent}/${player.healthTotal}"
+        )
+        Text(
+            text = "FP: ${player.focusCurrent}/${player.focusTotal}"
+        )
+        Text(
+            text = "Movement: ${player.movement}"
+        )
     }
 }
--- android/app/src/main/java/com/example/combattracker/model/Entity.kt
+++ android/app/src/main/java/com/example/combattracker/model/Entity.kt
@@ -2,7 +2,7 @@ package com.example.combattracker.model
 
 import kotlin.math.ceil
 
-class Entity(val attributes: Attributes, val movement: Int, private val healthFactor: Double, private val focusFactor: Double) {
+class Entity(val name: String, val attributes: Attributes, val movement: Int, private val healthFactor: Double, private val focusFactor: Double) {
     data class Attributes(val constitution: Int, val strength: Int, val dexterity: Int, val wisdom: Int, val intelligence: Int, val charisma: Int)
 
     val healthTotal = ceil(healthFactor * attributes.constitution).toInt()
